19fccd7cae19abc80f976131197898bd34e46299
VagnerSilva <vagner23silva@gmail.com>
2018-07-13 21:47:31 -0300

    config babel

---
+++ test/search.js
@@ -0,0 +1,10 @@
+
+import { expect } from 'chai';
+
+describe('Spotify', () => {
+  describe('Smoke test', () => {
+    it('Should exist the search, searchAlbums, searchArtists, searchTracks methods', () => {
+      expect(2);
+    });
+  });
+});
