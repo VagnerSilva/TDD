e62d738b58e0b2cd986d58164e18205f38928016
VagnerSilva <vagner23silva@gmail.com>
2018-07-14 22:13:34 -0300

    umd

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,8 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    // libraryTarget: 'umd',
+    // library: 'searchSpot',
   },
 
   module: {
